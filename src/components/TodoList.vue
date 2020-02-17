144cdc617d9c8b1b682c13a4ee7af174b634afce
Alexey <lbonanzza@gmail.com>
2020-02-17 23:50:44 +0300

    first commit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card
+          class="mx-auto"
+          max-width="344"
+          outlined
+          :elevation="24"
+          style="border-radius: 12px"
+  >
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="overline mb-4">OVERLINE</div>
+        <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
+      </v-list-item-content>
+
+
+    </v-list-item>
+
+    <v-card-actions >
+      <v-btn style="border-radius: 12px" text>Done</v-btn>
+      <v-btn style="border-radius: 12px" text>Delete</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
