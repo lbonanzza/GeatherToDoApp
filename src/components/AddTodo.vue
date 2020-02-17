5b7f129f5e57c1012897c9ce052a7707de4be953
Alexey <lbonanzza@gmail.com>
2020-02-17 23:56:10 +0300

    ProgressBar and AddTodo added

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container style="max-width: 500px">
+    <v-text-field v-model="title" label="What are you working on?" solo @keydown.enter="create"></v-text-field>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "AddTodo",
+  data() {
+    return {
+      title: ""
+    };
+  },
+  methods: {
+    create() {
+      if (this.title.trim()) {
+        const newTodo = {
+          id: Date.now(),
+          title: this.title,
+          completed: false
+        };
+
+        this.$emit("create-todo", newTodo);
+        this.title = "";
+      }
+    }
+  }
+};
+</script>
+
+<style lang="sass" scoped>
+
+</style>>
+ 
